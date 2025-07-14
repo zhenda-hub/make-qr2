79882c0914f1a9d6c1ce7a970b80731af6bfb32c
zhenda <msoft77564532@outlook.com>
2025-07-14 16:52:20 +0800

    remove: useless code

--- backend/app/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    DATABASE_URL: str
-    
-    model_config = SettingsConfigDict(case_sensitive=True)
-
-settings = Settings()
